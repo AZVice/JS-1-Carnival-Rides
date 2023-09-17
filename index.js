a9e5a93ef0e2b7fd2c1ce0e70f98bbef50e34b9d
Carlos Hernandez <chernandezmiranda@gmail.com>
2023-09-17 16:31:43 -0700

    incomplete excersise

--- index.js
+++ index.js
@@ -12,20 +12,31 @@ let waterIsOn = true
 //Ride 1 works when:
     // green wire is connected
 ride1PowerOn = false
+if (ride1PowerOn = greenWireConnected){
+
+}
 
 //Ride 2 works when 
     //green and yellow wires are connected
 ride2PowerOn = false
+if(ride2PowerOn = (greenWireConnected && yellowWireConnected)){
+
+}
 
 //Ride 3 works when:
     // green and red wires are connected
     // water is on 
     // yellow wire is not connected
 ride3PowerOn = false
+if(ride3PowerOn = (greenWireConnected && redWireConnected) && waterIsOn && yellowWireConnected){
+
+}
 
 //Ride 4 has power when:
-    //ride wire is connected
+    //red wire is connected
 ride4PowerOn = false
+if (ride4PowerOn = redWireConnected){
+}
 //Ride 4 is a slot machine
     //if a 1 is rolled it should return "star"
     //if a 2 is rolled it should return "car"
@@ -35,5 +46,25 @@ ride4PowerOn = false
     //if a 6 is rolled it should return "tiger"
     //if a 7 is rolled it should return "seven"
 function ride4SlotMachine(rolledNumber){
-    return "nothing"
+    return "nothing  "
+}
+
+let rolledNumber = 1;
+
+if (rolledNumber === "1"){
+    console.log("star");
+}else if( rolledNumber === 2){
+    console.log("car");
+}else if( rolledNumber === 3){
+    console.log("pot of gold");
+}else if( rolledNumber === 4){
+    console.log("clover");
+}else if( rolledNumber === 5){
+    console.log("bowling ball");
+}else if( rolledNumber === 6){
+    console.log("tiger");
+}else if( rolledNumber === 7){
+    console.log("seven");
+}else{
+    console.log("");
 }
